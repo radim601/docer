975fca8c96a5fe3c18752dbde59dd6fabeb1284a
Radomir <85487164+radim601@users.noreply.github.com>
2024-12-22 23:36:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -17,14 +17,33 @@ migrate = Migrate(app, db)
 #модель
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
-    def repr(self):
-        return f'<User {self.name}>'
+    def __repr__(self):
+        return f'<User {self.username}>'
 
+# Маршрут для добавления пользователя
+@app.route('/users', methods=['POST'])
+def add_user():
+    username = request.json.get('username')
+    if User.query.filter_by(username=username).first():
+        return jsonify({'error': 'User already exists'}), 400
+    
+    new_user = User(username=username)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'username': new_user.username}), 201
+
+# Маршрут для получения всех пользователей
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'username': user.username} for user in users]), 200
+    
 @app.route('/')
 def hello():
     return jsonify({'message': 'Hello from Flask!'})
+    
 
 if __name__ == '__main__':
     with app.app_context():
